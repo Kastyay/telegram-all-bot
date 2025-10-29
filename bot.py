de6c4e69a093756a55b4b88e99c030395ba1fc4a
Kastyay <113598144+Kastyay@users.noreply.github.com>
2025-10-29 18:00:10 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 import os
 import telebot
 
-BOT_TOKEN = os.environ['BOT_TOKEN']
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8443886410:AAECQfMTX4wVf0Ax1zkbVoqDbcUtMVTZIQU')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "🤖 Используйте /all чтобы упомянуть всех.")
+    bot.reply_to(message, "🤖 Используйте /all")
 
 @bot.message_handler(commands=['all'])
 def all_cmd(message):
     bot.send_message(message.chat.id, "Тегаю всех\n@mxvsatv @dnodnahadnedna @komi2021 @san_dor_ka @khopunovvv @Da1syBell @normal35235 @prowess100 @DaveTokami @m1st777 @Kastyay")
 
-print("✅ Бот запущен на Railway!")
+print("✅ Бот запущен!")
 bot.polling(none_stop=True)
